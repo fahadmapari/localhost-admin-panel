8c77023d3be68b8a1c6f358fcf0974efaa3fd90a
fahadmapari <btechy4@gmail.com>
2025-07-20 00:34:32 +0530

    feat: add SWR for data fetching in ProductList component; integrate API call for product data

--- src/pages/product/ProductList.tsx
+++ src/pages/product/ProductList.tsx
@@ -1,4 +1,6 @@
 import { DataTable } from "@/components/common/DataTable";
+import api from "@/lib/axios";
+import useSWR from "swr";
 
 const List = () => {
   const columns = [
@@ -20,8 +22,16 @@ const List = () => {
     },
   ];
 
+  const { data, error, isLoading } = useSWR("/products", async (url) => {
+    const { data } = await api.get(url);
+    return data?.data;
+  });
+
+  console.log(data);
+
   return (
     <div className="p-4">
+      <h1 className="text-3xl font-semibold pb-4">Product List</h1>
       <DataTable columns={columns} data={[]} />
     </div>
   );
