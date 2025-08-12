8fa0d9719cc8164141851d86bc246950713607d2
fahad mapari <btechy4@gmail.com>
2025-08-12 20:10:38 +0530

    admin register form

--- src/pages/admin/AdminRegister.tsx
+++ src/pages/admin/AdminRegister.tsx
@@ -1,12 +1,134 @@
 import PageHeading from "@/components/common/PageHeading";
+import DropdownSelect from "@/components/inputs/DropdownSelect";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { userSchema } from "@/schemas/user.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const AdminRegister = () => {
+  const form = useForm({
+    resolver: zodResolver(userSchema),
+  });
+
+  async function onSubmit(values: z.infer<typeof userSchema>) {}
+
   return (
-    <div className="p-4">
-      <div className="pb-40">
+    <div className="p-4 h-full flex flex-col">
+      <div className="pb-4">
         <PageHeading label="Register New Admin" />
       </div>
-      <div></div>
+      <div className="flex-1">
+        <Card className="w-full p-4 py-6">
+          <Form {...form}>
+            <form
+              className="flex flex-col gap-4"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <div className="flex items-start gap-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input {...field} type="text" placeholder="John Doe" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          type="email"
+                          placeholder="John@example.com"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="flex items-start gap-4">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input {...field} type="password" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Confirm Password</FormLabel>
+                      <FormControl>
+                        <Input {...field} type="password" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div>
+                <FormField
+                  control={form.control}
+                  name="role"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Admin Type</FormLabel>
+                      <FormControl>
+                        <DropdownSelect
+                          {...field}
+                          value={field.value}
+                          defaultValue={field.value}
+                          label="Admin Types"
+                          onChange={field.onChange}
+                          options={userSchema.shape.role.options}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <Button type="submit" className="w-full">
+                Register Admin
+              </Button>
+            </form>
+          </Form>
+        </Card>
+      </div>
     </div>
   );
 };
