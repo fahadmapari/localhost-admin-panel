9ec71314737f3624e8fc50ff1f9e940eb30f27ec
fahad mapari <btechy4@gmail.com>
2025-08-26 00:53:23 +0530

    refactor: simplify NewBooking layout and integrate PageHeading in BookingForm

--- src/pages/booking/NewBooking.tsx
+++ src/pages/booking/NewBooking.tsx
@@ -1,10 +1,8 @@
 import BookingForm from "@/components/booking/BookingForm";
-import PageHeading from "@/components/common/PageHeading";
 
 const NewBooking = () => {
   return (
-    <div className="h-full p-4 flex flex-col">
-      <PageHeading label="New Booking" />
+    <div className="h-full p-4">
       <BookingForm />
     </div>
   );
