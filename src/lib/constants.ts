13af422a9618aee8ed67a9a25670ba67b1529f70
fahad mapari <btechy4@gmail.com>
2025-09-02 00:59:02 +0530

    env added

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_API_URL = "http://localhost:5500/api/v1";
-export const SERVER_URL = "http://localhost:5500";
+export const BASE_API_URL = import.meta.env.VITE_BACKEND_API_URL;
+export const SERVER_URL = import.meta.env.VITE_BACKEND_URL;
 export const CLOUD_FRONT_URL = "https://d2teipbdqb7i3a.cloudfront.net/";
 export const THEME_KEY = "ui-theme";
